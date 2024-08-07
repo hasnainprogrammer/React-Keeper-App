308f0afa0bb05eab1fdc65d89456f474dabfb54d
hasnainprogrammer <hasnainprogrammer@gmail.com>
2024-08-07 21:50:43 +0500

    Keeper App

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import Note from "./Footer";
+import Footer from "./Note";
+
+const App = function () {
+  return (
+    <>
+      <Header />
+      <Note />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Note = function () {
+  return (
+    <div className="note">
+      <h1>Todo Title</h1>
+      <p>Todo description</p>
+    </div>
+  );
+};
+
+export default Note;
