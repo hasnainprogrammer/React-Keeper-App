e0e02fec22ad142faeff689a1ceafd7420ec23e3
Hasnain Khan <103248723+hasnainprogrammer@users.noreply.github.com>
2024-09-11 22:23:28 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,33 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./components/App";
+import React, {useState} from 'react'
+import ReactDOM from 'react-dom'
 
-const root = createRoot(document.getElementById("root"));
-root.render(<App />);
+function App() {
+  const [todoVal, setTodoVal] = useState('')
+  const [todos, setTodos] = useState([])
+  function handleChange(e) {
+    e.preventDefault()
+    setTodoVal(e.target.value)
+  }
+  function handleClick() {
+    setTodos((prev) => {
+      return [...prev, todoVal]
+    })
+    setTodoVal('')
+  }
+  return (
+    <>
+      <h2>Simple Todo App</h2>
+      <hr />
+      <br />
+      <input type="text" onChange={handleChange} value={todoVal} />
+      <br />
+      <br />
+      <button onClick={handleClick}>Add Todo</button>
+      <h3>Your Todo's</h3>
+      {todos.map((todo) => {
+        return <li>{todo}</li>
+      })}
+    </>
+  )
+}
+ReactDOM.render(<App />, document.getElementById('root'))
